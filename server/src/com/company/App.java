141ccbbdd79efceedc7b9e65843503c6da6e58c8
mmmisty <taluo.mist@gmail.com>
2015-11-30 00:49:56 -0600

    no input for server

--- server/src/com/company/App.java
+++ server/src/com/company/App.java
@@ -24,43 +24,43 @@ public class App {
         }
         _server.Start();
 
-        while (true) {
-            // TODO read and process command here
-            try {
-                Scanner s1 = new Scanner(System.in);
-//                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                System.out.println("Input command, e.g., send NavyPier 41.8916241 -87.6094798");
-//                String s = br.readLine();
-                String s = s1.nextLine();
-
-                if (s.equals("exit")) {
-                    break;
-                }
-
-                String[] ss = s.split(" ");
-                if (ss[0].equals("send")) {
-                    if (ss.length <= 4) {
-                        System.out.println("Invalid args");
-                        continue;
-                    }
-                    try {
-                        double d1 = Double.parseDouble(ss[2]);
-                        double d2 = Double.parseDouble(ss[3]);
-                    } catch (NumberFormatException e) {
-                        System.out.println("Invalid coordinates.");
-                        continue;
-                    }
-                    _server.SendToAll("Emergency", s.substring(5, s.length()));
-                } else {
-                    System.out.println("Invalid command.");
-                }
-                //TODO process
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                break;
-            }
-        }
+//        while (true) {
+//            // TODO read and process command here
+//            try {
+//                Scanner s1 = new Scanner(System.in);
+////                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//                System.out.println("Input command, e.g., send NavyPier 41.8916241 -87.6094798");
+////                String s = br.readLine();
+//                String s = s1.nextLine();
+//
+//                if (s.equals("exit")) {
+//                    break;
+//                }
+//
+//                String[] ss = s.split(" ");
+//                if (ss[0].equals("send")) {
+//                    if (ss.length <= 4) {
+//                        System.out.println("Invalid args");
+//                        continue;
+//                    }
+//                    try {
+//                        double d1 = Double.parseDouble(ss[2]);
+//                        double d2 = Double.parseDouble(ss[3]);
+//                    } catch (NumberFormatException e) {
+//                        System.out.println("Invalid coordinates.");
+//                        continue;
+//                    }
+//                    _server.SendToAll("Emergency", s.substring(5, s.length()));
+//                } else {
+//                    System.out.println("Invalid command.");
+//                }
+//                //TODO process
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                break;
+//            }
+//        }
 
         _server.Stop();
     }
